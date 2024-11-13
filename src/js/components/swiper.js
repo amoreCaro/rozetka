23ec90379af70a9f4dea9c6ea88c4de54dadace2
mac <mac@MacBook-Pro-3.local>
2024-11-13 20:25:39 +0200

    swiper js

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,20 +1,12 @@
-import Swiper from 'swiper/bundle';
-import '../../scss/components/swiper.scss'; // ваші кастомні стилі
-
-const swiper = new Swiper('.swiper', {
-    modules: [Pagination, Navigation],
-    speed: 500,
-    slidesPerView: 1,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
+var swiper = new Swiper(".swiper__container", {
+    cssMode: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
 });
-
-export default swiper;
