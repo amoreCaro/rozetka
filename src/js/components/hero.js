4bf303b5751ee3fbe6c4c7d7ba760838b406d7ab
mac <mac@MacBook-Pro-3.local>
2024-11-15 20:29:04 +0200

    added autoplay for slider

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,6 +1,10 @@
 export default function Hero() {
     return new Swiper(".hero__slider", {
         spaceBetween: 30,
+        autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
