5f97ead438274aca1067a719af3191344c36e0a6
mac <mac@MacBook-Pro-3.local>
2024-11-15 10:04:31 +0200

    hero buttons

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,6 +1,10 @@
 export default function Hero() {
     return new Swiper(".hero__slider", {
         spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
