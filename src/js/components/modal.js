f41fd1cb6cec8e543efb8a89fcc533d7491b47b5
mac <mac@MacBook-Pro-3.local>
2024-11-15 09:41:34 +0200

    hero button group

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,25 +1,37 @@
 function modal(modalId, openBtnId, closeBtnId) {
-    const modal = document.getElementById(modalId);
-    const openModalBtn = document.getElementById(openBtnId);
-    const closeModalBtn = document.getElementById(closeBtnId);
+    document.addEventListener("DOMContentLoaded", () => {
+        const modal = document.getElementById(modalId);
+        const openModalBtn = document.getElementById(openBtnId);
+        const closeModalBtn = document.getElementById(closeBtnId);
 
-    if (openModalBtn) {
-        openModalBtn.onclick = () => {
-            if (modal) modal.style.display = "block";
-        };
-    }
-
-    if (closeModalBtn) {
-        closeModalBtn.onclick = () => {
-            if (modal) modal.style.display = "none";
-        };
-    }
-
-    window.onclick = (event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
+        if (!modal) {
+            console.error(`Модальное окно с ID "${modalId}" не найдено.`);
+            return;
         }
-    };
+        
+        if (openModalBtn) {
+            openModalBtn.onclick = () => {
+                modal.style.display = "block";
+            };
+        } else {
+            console.error(`Кнопка открытия модалки с ID "${openBtnId}" не найдена.`);
+        }
+
+        if (closeModalBtn) {
+            closeModalBtn.onclick = () => {
+                modal.style.display = "none";
+            };
+        } else {
+            console.error(`Кнопка закрытия модалки с ID "${closeBtnId}" не найдена.`);
+        }
+
+        // Закрытие модалки при клике за ее пределами
+        window.onclick = (event) => {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        };
+    });
 }
 
 export default modal;
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import modal from "./components/modal";
+import hero from './components/hero';
+// import modal from "./components/modal";
 // import sidebar from "./components/sidebar";
 // import setupMegaMenu from "./components/setupMegaMenu";
-import hero from './components/hero';
 // import toggleMenuVisibility from "./components/toggleMenuVisibility";
 // import phoneInput from "./components/phoneInput";
 
 document.addEventListener("DOMContentLoaded", function() {
-    modal();
+    hero();
+    // modal();
     // sidebar();
     // setupMegaMenu();
-    hero();
     // toggleMenuVisibility();
     // phoneInput();
 });
