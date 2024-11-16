ff3841555f5fd197529939bb6b320f3ae7b8510c
mac <mac@MacBook-Pro-3.local>
2024-11-16 13:35:22 +0200

    processing

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
-// import hero from './components/hero';
-// import sidebar from "./components/sidebar";
-// import modal from "./components/modal";
-// import setupMegaMenu from "./components/setupMegaMenu";
-// import toggleMenuVisibility from "./components/toggleMenuVisibility";
-// import phoneInput from "./components/phoneInput";
-import phoneInput from "./components/langMenu";
+import hero from './components/hero';
+import sidebar from "./components/sidebar";
+import modal from "./components/modal";
+import setupMegaMenu from "./components/setupMegaMenu";
+import toggleMenuVisibility from "./components/toggleMenuVisibility";
+import phoneInput from "./components/phoneInput";
+import langMenu from "./components/langMenu";
 
 document.addEventListener("DOMContentLoaded", function() {
-    // hero();
-    // modal();
-    // sidebar();
-    // setupMegaMenu();
-    // toggleMenuVisibility();
-    // phoneInput();
+    hero();
+    modal();
+    sidebar();
+    setupMegaMenu();
+    toggleMenuVisibility();
+    phoneInput();
     langMenu();
 });
