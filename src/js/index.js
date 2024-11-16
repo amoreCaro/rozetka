d236afee0d576ad6de16a3d97f6bdcd7d977ea45
mac <mac@MacBook-Pro-3.local>
2024-11-16 13:30:51 +0200

    developing lang-menu-dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import hero from './components/hero';
-import sidebar from "./components/sidebar";
-import modal from "./components/modal";
-import setupMegaMenu from "./components/setupMegaMenu";
-import toggleMenuVisibility from "./components/toggleMenuVisibility";
-import phoneInput from "./components/phoneInput";
+// import hero from './components/hero';
+// import sidebar from "./components/sidebar";
+// import modal from "./components/modal";
+// import setupMegaMenu from "./components/setupMegaMenu";
+// import toggleMenuVisibility from "./components/toggleMenuVisibility";
+// import phoneInput from "./components/phoneInput";
 
 document.addEventListener("DOMContentLoaded", function() {
-    hero();
-    modal();
-    sidebar();
-    setupMegaMenu();
-    toggleMenuVisibility();
-    phoneInput();
+    // hero();
+    // modal();
+    // sidebar();
+    // setupMegaMenu();
+    // toggleMenuVisibility();
+    // phoneInput();
 });
