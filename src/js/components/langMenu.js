8eac4153858d2c64d2660299a42b168b3fb88e78
mac <mac@MacBook-Pro-3.local>
2024-11-16 14:17:13 +0200

    open lang menu logic

--- src/js/components/langMenu.js
+++ src/js/components/langMenu.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.querySelector('.open-lang-menu-btn');
+    const langMenuContainer = document.querySelector('.lang-menu-container');
     const buttons = document.querySelectorAll(".lang-menu__item");
+    
+    toggleBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        langMenuContainer.classList.toggle('active');
+    });
+    
+    document.addEventListener('click', (e) => {
+    
+        if (!langMenuContainer.contains(e.target) && !toggleBtn.contains(e.target)) {
+            langMenuContainer.classList.remove('active');
+        }
+    });
+    
+    langMenuContainer.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
 
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
