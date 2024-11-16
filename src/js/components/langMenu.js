9c06b528feeee1a83629a9ff0944ce00f6fdee2a
mac <mac@MacBook-Pro-3.local>
2024-11-16 14:33:21 +0200

    lang menu

--- src/js/components/langMenu.js
+++ src/js/components/langMenu.js
@@ -2,29 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggleBtn = document.querySelector('.open-lang-menu-btn');
     const langMenuContainer = document.querySelector('.lang-menu-container');
     const buttons = document.querySelectorAll(".lang-menu__item");
-    
+
     toggleBtn.addEventListener('click', (e) => {
-        e.stopPropagation();
-        langMenuContainer.classList.toggle('active');
+        e.stopPropagation(); 
+        langMenuContainer.classList.toggle('active'); 
     });
-    
+
     document.addEventListener('click', (e) => {
-    
         if (!langMenuContainer.contains(e.target) && !toggleBtn.contains(e.target)) {
-            langMenuContainer.classList.remove('active');
+            langMenuContainer.classList.remove('active'); 
         }
-    });
-    
-    langMenuContainer.addEventListener('click', (e) => {
-        e.stopPropagation();
     });
 
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
-            // Видаляємо клас 'active' у всіх кнопок
             buttons.forEach((btn) => btn.classList.remove("active"));
-
-            // Додаємо клас 'active' до обраної кнопки
             button.classList.add("active");
         });
     });
